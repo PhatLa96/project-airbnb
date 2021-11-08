6a4ab96c6c292b7e8d06b5343cacd219bd75a9d6
Phát Lã <lavantuanphat15@gmail.com>
2021-11-08 09:41:01 +0700

    first commit

---
+++ src/pages/Detail/Rating/ContentUserRating/index.jsx
@@ -0,0 +1,39 @@
+import { Button, Typography } from "@material-ui/core";
+import moment from "moment";
+import React, { Fragment } from "react";
+import { MdNavigateNext } from "react-icons/md";
+import useStyles from "./style";
+const ContentUserRating = ({ item, handleShowModal, openModal }) => {
+  const classes = useStyles();
+  return (
+    <Fragment>
+      <div className={classes.rating__infoUser}>
+        <img
+          src={item?.userId?.avatar}
+          alt="avatar"
+          className={classes.rating_avatar}
+        />
+        <div className={classes.rating__createAt}>
+          <Typography variant="body2">{item?.userId?.name}</Typography>
+          <Typography variant="span">
+            {moment(`${item?.created_at}`).format("Do MMM YYYY hh:mm")}
+          </Typography>
+        </div>
+      </div>
+      <Typography variant="span" className={classes.rating__content}>
+        {item?.content?.length > 160 && !openModal ? (
+          <Fragment>
+            {item?.content?.substr(0, 140)}...
+            <Button onClick={handleShowModal}>
+              Hiển thị thêm <MdNavigateNext />
+            </Button>
+          </Fragment>
+        ) : (
+          item?.content
+        )}
+      </Typography>
+    </Fragment>
+  );
+};
+
+export default ContentUserRating;
